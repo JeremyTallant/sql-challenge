56e054535eb9d1a83d0a44531545deed50807436
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2022-11-19 09:22:31 -0600

    finished table_schemata.sql file

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,12 +1,67 @@
 -- Drop table if exists
-drop table if exists titles;
-drop table if exists employees;
-drop table if exists dept_emp;
-drop table if exists salaries;
-drop table if exists dept_manager;
-drop table if exists departments;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS departments;
 
 -- Create titles table
-create table titles (
-	title_id varchar NOT NULL,
-	)
+CREATE TABLE titles (
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+);
+	
+-- Create employees table
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id) 
+);
+
+-- Create salaries table
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+-- Create departments table
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+-- Create dept_emp table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no,dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- Create dept_manager table
+CREATE TABLE dept_manager ( 
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no,emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- View tables
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM salaries;
+SELECT * FROM dept_manager;
+SELECT * FROM departments;
