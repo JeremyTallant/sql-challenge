378a0aab3e4b1c85ecec18ad4839ba962600dd9e
JeremyTallant <tallantj95@yahoo.com>
2024-01-16 08:13:09 -0700

    added the 5th query

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -19,3 +19,9 @@ SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 JOIN dept_emp de ON e.emp_no = de.emp_no
 JOIN departments d ON de.dept_no = d.dept_no;
+
+-- 5. Employees Named Hercules with Last Names Starting with B
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
